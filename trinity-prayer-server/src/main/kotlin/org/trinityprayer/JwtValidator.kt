42af4f1cc04ef92d9e709817c6e66ab2fcf80897
Erfang Chen <erfangc@gmail.com>
2025-04-05 09:41:27 -0500

    Add initial models and API controller for prayer intentions
    Introduced `PrayerIntention` model and a scaffolded `TrinityPrayerController` with a basic API setup. Removed unused `main` method and Jackson dependency from `JwtValidator` for cleanup and alignment with new design.

--- trinity-prayer-server/src/main/kotlin/org/trinityprayer/JwtValidator.kt
+++ trinity-prayer-server/src/main/kotlin/org/trinityprayer/JwtValidator.kt
@@ -4,7 +4,6 @@ import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import com.auth0.jwt.exceptions.JWTVerificationException
 import com.auth0.jwt.interfaces.DecodedJWT
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.stereotype.Service
 
 @Service
@@ -22,17 +21,4 @@ class JwtValidator {
         return verifier.verify(token)
     }
 
-}
-
-fun main() {
-    val decodedJWT = JwtValidator()
-        .validate("eyJhbGciOiJIUzI1NiIsImtpZCI6IlZWaUdoLzY1ZWIxWGZLbVkiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2R4YmVwZW9zYWZncXdvcmN1eWN6LnN1cGFiYXNlLmNvL2F1dGgvdjEiLCJzdWIiOiJiYjAxNmI1YS0zNmVkLTQ3ZDYtYWYxZi1kOWFiYzQxNGE1Y2EiLCJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzQzODY1MTQ3LCJpYXQiOjE3NDM4NjE1NDcsImVtYWlsIjoiZXJmYW5nY0BnbWFpbC5jb20iLCJwaG9uZSI6IiIsImFwcF9tZXRhZGF0YSI6eyJwcm92aWRlciI6ImVtYWlsIiwicHJvdmlkZXJzIjpbImVtYWlsIl19LCJ1c2VyX21ldGFkYXRhIjp7ImVtYWlsIjoiZXJmYW5nY0BnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiZmlyc3RfbmFtZSI6IkVyZmFuZyIsImxhc3RfbmFtZSI6IkNoZW4iLCJwaG9uZV92ZXJpZmllZCI6ZmFsc2UsInN1YiI6ImJiMDE2YjVhLTM2ZWQtNDdkNi1hZjFmLWQ5YWJjNDE0YTVjYSJ9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJhbXIiOlt7Im1ldGhvZCI6InBhc3N3b3JkIiwidGltZXN0YW1wIjoxNzQzODYxNTQ3fV0sInNlc3Npb25faWQiOiJiOTBjYThmZS00OTNmLTQ1NjctYjU2NC0yM2RhNmZkMGIwYzEiLCJpc19hbm9ueW1vdXMiOmZhbHNlfQ.rHYkhDR8oxm2wy7wN5bLdWetXZSpKMVV6GwxKd7r928")
-    println(decodedJWT.token)
-    println(decodedJWT.payload)
-    println(decodedJWT.claims)
-    val userProvider = UserProvider(jacksonObjectMapper())
-    userProvider.setUser(decodedJWT.token)
-    val supabaseJwtPayload = userProvider.getUser()
-    println(supabaseJwtPayload)
-    println(supabaseJwtPayload?.userMetadata?.firstName)
 }
---
+++ trinity-prayer-server/src/main/kotlin/org/trinityprayer/Models.kt
@@ -0,0 +1,6 @@
+package org.trinityprayer
+
+data class PrayerIntention(
+    private val id: Long,
+    private val intentText: String
+)
