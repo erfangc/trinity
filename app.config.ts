c4a97f542a99f0a5f85acb0e83addf28acc82942
Erfang Chen <erfangc@gmail.com>
2025-04-23 10:11:39 -0500

    Update app version to 1.1.1 in configuration
    This version bump reflects minor changes or fixes applied to the app. Ensure all dependencies and features align with this updated version.

--- app.config.ts
+++ app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         ...config,
         name: "Trinity Prayers",
         slug: "trinity",
-        version: "1.1.0",
+        version: "1.1.1",
         orientation: "portrait",
         icon: "./assets/images/icon.png",
         scheme: "trinity",
